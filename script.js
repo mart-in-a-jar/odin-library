9f31ed4f7e7d291d4922ad59c623657fe0fb5f28
Martin <Martin@Jarneid.no>
2022-05-23 13:25:00 +0200

    Add localstorage get/put and hotkeys

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const library = [];
+const library = JSON.parse(localStorage.getItem("library"));
 const table = document.querySelector("table.library tbody");
 const addBookModal = document.querySelector(".form.modal");
 const blurred = document.querySelector(".blur");
 const addBookButton = document.querySelector("header button.add");
+const searchField = document.querySelector("input[type='search'");
+
+writeTable();
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -27,6 +30,9 @@ function hideModal() {
     addBookModal.classList.remove("active");
     blurred.classList.remove("active");
     clearForm();
+    document.querySelectorAll("form input").forEach(input => {
+        input.blur();
+    });
 }
 
 function addBookToLibrary() {
@@ -35,8 +41,9 @@ function addBookToLibrary() {
     const pages = document.querySelector("#pages").value;
     const read = document.querySelector("#read").checked;
 
-    //Returns the number of books in the library
-    return library.push(new Book(title, author, pages, read));
+    const newBook = new Book(title, author, pages, read);
+    library.push(newBook);
+    putLocal();
 }
 
 function clearForm() {
@@ -48,7 +55,8 @@ function clearForm() {
 
 document.querySelector("form.add-book").addEventListener("submit", (e) => {
     e.preventDefault();
-    updateTable(addBookToLibrary() - 1);
+    addBookToLibrary();
+    updateTable(library.length - 1);
     hideModal();
     clearForm();
 });
@@ -61,12 +69,26 @@ blurred.addEventListener("click", () => {
     hideModal();
 });
 
-window.addEventListener("keydown", e => {
+window.addEventListener("keyup", e => {
     if (blurred.classList.contains("active")) {
         if (e.key === "Escape") {
             hideModal();
         }
-    };
+    // Press "a" to add new book or "s" to search
+    } else {
+        if (!(searchField === document.activeElement) && !(e.ctrlKey || e.shiftKey)) {
+            if (e.key.toLowerCase() === "a") {
+            showModal();
+            } else if (e.key.toLowerCase() === "s") {
+                searchField.focus();
+            }
+        } else {
+            if (e.key === "Escape") {
+                searchField.blur();
+                searchField.value = "";
+            }
+        }
+    }
 });
 
 function writeTable() {
@@ -88,6 +110,7 @@ function writeTableRow(book) {
     const readCheck = document.createElement("input");
     readCheck.type = "checkbox";
     readCheck.dataset.libraryIndex = library.indexOf(book);
+    readCell.dataset.libraryIndex = library.indexOf(book);
 
     titleCell.textContent = book.title;
     authorCell.textContent = book.author;
@@ -100,15 +123,19 @@ function writeTableRow(book) {
 
 
     [titleCell, authorCell, pagesCell, readCell].forEach(cell => {
+        cell.dataset.libraryIndex = library.indexOf(book);
         tableRow.appendChild(cell);
     });
     table.appendChild(tableRow);
 }
 
-
+function putLocal() {
+    localStorage.setItem("library", JSON.stringify(library));
+}
 
 
 /*
-queryselectorall on checkboxes in table. change toggles library[data-id].toggleRead prototype
-
+    queryselectorall on checkboxes in table. change toggles library[data-id].toggleRead prototype
+    click book in table to view, delete etc.
+    save and load from localstorage
 */
